d38300254584a4beb3fd6a8cf4d0aa1fd18c38ae
joaocairiton <joaocairiton@gmail.com>
2023-07-28 03:27:11 -0300

    update project

--- Exercicicio bootstrap/js/main.js
+++ Exercicicio bootstrap/js/main.js
@@ -1,6 +1,15 @@
 let button = document.getElementById('show')
 let modal = document.getElementById('dialog')
+const closeButton = document.querySelector('dialog button')
 
-button.addEventListener('click', () =>{
+/*button.addEventListener('click', () =>{
     modal.showModal()
-})
+})*/
+
+button.onclick = function () {
+    modal.show()
+    
+}
+closeButton.onclick = function () {
+    modal.close()
+}
