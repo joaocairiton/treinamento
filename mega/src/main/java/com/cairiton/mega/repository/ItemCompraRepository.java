6e51a67d0ecec610f4b6087b8795ccb2d32b7561
joaocairiton <43797301+joaocairiton@users.noreply.github.com>
2023-09-21 17:48:36 -0300

    Delete mega directory

--- mega/src/main/java/com/cairiton/mega/repository/ItemCompraRepository.java
+++
@@ -1,9 +0,0 @@
-package com.cairiton.mega.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.cairiton.mega.model.ItemCompra;
-
-public interface ItemCompraRepository extends JpaRepository<ItemCompra, Integer>{
-
-}
