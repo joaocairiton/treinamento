241e0e1db600701a5765838d8127d952a8c0c4bf
joaocairiton <43797301+joaocairiton@users.noreply.github.com>
2023-09-25 10:31:47 -0300

    Delete Exercicio_SQL_scripts directory

--- Exercicio_SQL_scripts/item-compra/insert-de-item-compras.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO itemCompra (nome, valor_item, codigo_compra)
-VALUES ('arroz', 25, 1),
-       ('feijao', 15, 2),
-       ('azeite', 31, 3),
-       ('arroz', 25, 1),
-       ('oleo', 250, 4),
-       ('pneu', 362, 5)
